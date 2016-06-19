8f72cd16f923b14f38eae8d7e8487bd6f4f5296b
Magicshui <magicshui@users.noreply.github.com>
2016-06-19 09:43:21 +0800

    添加“编辑菜单组”
    R.T，这样可以使用复制、粘贴等快捷键

--- src/menu.js
+++ src/menu.js
@@ -34,6 +34,44 @@ var template = [
         ]
     },
     {
+        label: '编辑',
+        submenu: [
+            {
+                label: '撤销',
+                accelerator: 'CmdOrCtrl+Z',
+                role: 'undo'
+            },
+            {
+                label: '重做',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: '剪切',
+                accelerator: 'CmdOrCtrl+X',
+                role: 'cut'
+            },
+            {
+                label: '复制',
+                accelerator: 'CmdOrCtrl+C',
+                role: 'copy'
+            },
+            {
+                label: '粘贴',
+                accelerator: 'CmdOrCtrl+V',
+                role: 'paste'
+            },
+            {
+                label: '全选',
+                accelerator: 'CmdOrCtrl+A',
+                role: 'selectall'
+            }
+        ]
+    },
+    {
         label: '运行',
         submenu: [
             {
@@ -190,4 +228,3 @@ if (process.platform == 'darwin') {
 
 var menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
-
