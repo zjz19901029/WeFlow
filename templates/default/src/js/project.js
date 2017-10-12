c9bc10bfca6ae701893a3bae1d5b043d2fd0c904
周静之 <mirror.zhou@platenogroup.com>
2017-10-12 09:38:41 +0800

    增加 酒店跳转的 环境自动切换

--- templates/default/src/js/project.js
+++ templates/default/src/js/project.js
@@ -5,6 +5,9 @@ $(function() {
     WeUI.Config.wx_share_host = HOST.wx_share;
     WeUI.Config.login.wx_oauth = HOST.wx_oauth;
     WeUI.Config.login.wx_login = HOST.wx_login;
+
+    WeUI.Config.hotel.app.href_v2 = HOST.hotelDetail;
+    WeUI.Config.hotellist.app.href_v2 = HOST.hotelList;
     
     WeUI.Util.setPageInfo({//设置分享
         title: document.title,
